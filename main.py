d4b559e79589360bcd9c33c99043727f7b486ef8
source <marco@cakebox.net>
2021-11-02 08:05:35 +0100

    All parameters should now be represented

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class resp_gui( QtWidgets.QMainWindow ):
 		self.ui = loader.load(ui_file, self)
 		ui_file.close()
 		self.ui.show()
-		self.show_ro = True
 
 		self.current_values = {}
 		self.update_current_values()
@@ -30,9 +29,13 @@ class resp_gui( QtWidgets.QMainWindow ):
 		self.ui.listWidget.clear()
 		self.ui.listWidget.setAlternatingRowColors(True)
 
-		#         0    1      2     3    4     5    6
-		# name: (id, offset, type, max, min , r/w, info)
 		for param in self.current_values:
+
+			tmp_param_info = list( self.current_values[param]['defaults'] )
+			for i in range(0,7):
+				tmp_param_info.pop(0)
+			cur_val = self.current_values[param]['cur_val']
+
 			if self.current_values[param]['defaults'][5] == 'rw':
 				if self.current_values[param]['defaults'][2] == 'int' and self.current_values[param]['defaults'][3] == 1:
 					item = QtWidgets.QListWidgetItem()
@@ -41,21 +44,40 @@ class resp_gui( QtWidgets.QMainWindow ):
 					item.setData( QtCore.Qt.UserRole, param )
 
 					info_label = QtWidgets.QLabel( self.current_values[param]['defaults'][6] )
-					#tmp_opts = ""
-					#print(type(self.current_values[param]['defaults']))
-					#for i in range(1, len( self.current_values[param]['defaults'] ) - 7 ):
-					#	tmp_opts = f"{self.current_values[param]['defaults'][i+6]}\n"
-					#info_label.setToolTip(tmp_opts)
 					layout.addWidget(info_label)
 
 					chkBox = QtWidgets.QCheckBox()
-					if self.current_values[param]['cur_val'] == 1:
+					if cur_val == 1:
 						chkBox.setChecked( True )
 					chkBox.setFixedWidth( 24 )
-					#btn_on.clicked.connect( lambda state=True, x=strand['host']: self.configured_strand_btn_on(x) )
+					chkBox.stateChanged.connect( lambda state=True, x=[param,chkBox]: self.update_bool_parameter(x) )
+					tmp_opts = ""
+					for i in tmp_param_info:
+						tmp_opts += f"{i}\n"
+					chkBox.setToolTip(tmp_opts)
 					layout.addWidget(chkBox)
 
-					
+					widget.setLayout(layout)
+
+					self.ui.listWidget.addItem( item )
+					item.setSizeHint( widget.sizeHint() )
+					self.ui.listWidget.setItemWidget( item, widget )
+
+				if self.current_values[param]['defaults'][2] == 'int' and self.current_values[param]['defaults'][3] >= 2:
+					item = QtWidgets.QListWidgetItem()
+					widget = QtWidgets.QWidget()
+					layout = QtWidgets.QHBoxLayout()
+					item.setData( QtCore.Qt.UserRole, param )
+
+					info_label = QtWidgets.QLabel( self.current_values[param]['defaults'][6] )
+					layout.addWidget(info_label)
+
+					combobox = QtWidgets.QComboBox()
+					for i in tmp_param_info:
+						combobox.addItem(i)
+					combobox.setCurrentIndex( cur_val )
+					combobox.currentIndexChanged.connect( lambda state=True, x=[param,combobox]: self.update_val_parameter(x) )
+					layout.addWidget( combobox )
 
 					widget.setLayout(layout)
 
@@ -71,25 +93,22 @@ class resp_gui( QtWidgets.QMainWindow ):
 
 					info_label = QtWidgets.QLabel( self.current_values[param]['defaults'][6] )
 					info_label.setWordWrap(True)
-					#tmp_opts = ""
-					#print(type(self.current_values[param]['defaults']))
-					#for i in range(1, len( self.current_values[param]['defaults'] ) - 7 ):
-					#	tmp_opts = f"{self.current_values[param]['defaults'][i+6]}\n"
-					#info_label.setToolTip(tmp_opts)
 					layout.addWidget(info_label)
-
 
 					float_val = QtWidgets.QDoubleSpinBox()
 					
 					float_val.setFixedWidth( 192 )
 					float_val.setSingleStep(0.00001)
-					float_val.setDecimals(len(str(self.current_values[param]['cur_val']).split(".")[1]))
+					float_val.setDecimals(len(str(cur_val).split(".")[1]))
 					float_val.setMaximum( self.current_values[param]['defaults'][3] )
 					temp_tt = f"Maximum: {self.current_values[param]['defaults'][3]}\nMinimum: {self.current_values[param]['defaults'][4]}"
 					float_val.setToolTip(temp_tt)
-					float_val.setValue( self.current_values[param]['cur_val'] )
-					float_val.valueChanged.connect( lambda state=True, x=param: self.update_float_parameter(x) )
-					#btn_on.clicked.connect( lambda state=True, x=strand['host']: self.configured_strand_btn_on(x) )
+					float_val.setValue( cur_val )
+					float_val.valueChanged.connect( lambda state=True, x=[param,float_val]: self.update_float_parameter(x) )
+					tmp_opts = ""
+					for i in tmp_param_info:
+						tmp_opts += f"{i}\n"
+					float_val.setToolTip(tmp_opts)
 					layout.addWidget(float_val)
 
 
@@ -100,32 +119,32 @@ class resp_gui( QtWidgets.QMainWindow ):
 					self.ui.listWidget.setItemWidget( item, widget )
 
 			elif self.current_values[param]['defaults'][5] == 'ro':
-				print(self.current_values[param])
-				if self.show_ro == True:
-					item = QtWidgets.QListWidgetItem()
-					widget = QtWidgets.QWidget()
-					layout = QtWidgets.QHBoxLayout()
-					item.setData( QtCore.Qt.UserRole, param )
+				item = QtWidgets.QListWidgetItem()
+				widget = QtWidgets.QWidget()
+				layout = QtWidgets.QHBoxLayout()
+				item.setData( QtCore.Qt.UserRole, param )
 
-					info_label = QtWidgets.QLabel( self.current_values[param]['defaults'][6] )
-					info_label.setWordWrap(True)
-					layout.addWidget(info_label)
+				info_label = QtWidgets.QLabel( self.current_values[param]['defaults'][6] )
+				info_label.setWordWrap(True)
+				layout.addWidget(info_label)
 
-					read_only_value = QtWidgets.QLabel()
-					ro_val = ""
-					if self.current_values[param]['cur_val'] == 1:
-						ro_val = "Enabled"
-					else:
-						ro_val = "Disabled"
-					read_only_value.setText(ro_val)
-					layout.addWidget( read_only_value )
+				read_only_value = QtWidgets.QLabel()
+				ro_val = ""
+				if cur_val == 1:
+					ro_val = "Enabled"
+				else:
+					ro_val = "Disabled"
+				read_only_value.setText(ro_val)
+				tmp_opts = ""
+				for i in tmp_param_info:
+					tmp_opts += f"{i}\n"
+				read_only_value.setToolTip(tmp_opts)
+				layout.addWidget( read_only_value )
+				widget.setLayout(layout)
 
-
-					widget.setLayout(layout)
-
-					self.ui.listWidget.addItem( item )
-					item.setSizeHint( widget.sizeHint() )
-					self.ui.listWidget.setItemWidget( item, widget )
+				self.ui.listWidget.addItem( item )
+				item.setSizeHint( widget.sizeHint() )
+				self.ui.listWidget.setItemWidget( item, widget )
 
 	def update_current_values( self ):
 		dev = res_tuning.find()
@@ -133,11 +152,14 @@ class resp_gui( QtWidgets.QMainWindow ):
 			temp_current_val = dev.read(name)
 			self.current_values[ name ] = {"defaults": res_tuning.PARAMETERS[name], "cur_val": temp_current_val }
 
-	def update_bool_parameter( self ):
-		pass
+	def update_bool_parameter( self, par_name ):
+		print(par_name[0], int(par_name[1].isChecked()))
 
-	def update_float_parameter( self, v1 ):
-		print(v1)
+	def update_val_parameter( self, par_name ):
+		print(par_name[0], par_name[1].currentIndex())
+
+	def update_float_parameter( self, par_name ):
+		print(par_name[0], par_name[1].value())
 
 if __name__ == '__main__':
 
